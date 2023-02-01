2ecb80c5a53e6b7d875fdded75fa1278f978f7f7
mateo <mateocc2013@gmail.com>
2023-02-01 15:33:06 +0100

    Initial commit

---
+++ Coding.py
@@ -0,0 +1,5 @@
+print ("This is some coding for homework", end="Homework")
+print ("This is code for homework")
+print ("this", "can", "be", "written", "for", "homework")
+#This is not a useful comment
+print("this", "can", "be", "written", "for", "homework", sep="!b ?")
